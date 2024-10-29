56532538240d18545af26ab070d4a6cf9fb1f102
NgoTanVuong1706 <vuongntps31613@fpt.edu.vn>
2024-10-29 07:42:15 +0700

    Add files via upload

---
+++ ps31613/dist/app.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    var targetId = this.getAttribute('href').substring(1);
+    var targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+var contactForm = document.querySelector('.contact-form');
+contactForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Cảm ơn bạn đã liên hệ với chúng tôi!');
+  contactForm.reset();
+});
